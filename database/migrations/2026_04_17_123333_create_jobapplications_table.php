7b1c7558c66a9e2841301622ee19fdb34648377d
ycaignacio <ignacioyohairra15@gmail.com>
2026-04-17 20:50:11 +0800

    Remove Job model, controller, and migration; add JobApplication model, controller, and migration

--- database/migrations/2026_04_17_123333_create_jobapplications_table.php
+++ database/migrations/2026_04_17_123333_create_jobapplications_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::create('job_applications', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('job_applications');
     }
 };
